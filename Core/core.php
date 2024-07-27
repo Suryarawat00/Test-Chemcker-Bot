d25aa4889a5cde709b274fb884d11329bf9eedb4
Suryarawat00 <rawatsurya17@gmail.com>
2024-07-27 14:39:36 +0530

    core.php

--- Core/core.php
+++ Core/core.php
@@ -1,6 +1,6 @@
 <?php
 
-$botToken =  "5980177112:AAF2saZaYIej03yT5_UE3Gbjlpgi1g7tnyw";
+$botToken =  "7495132028:AAHMrEA4l3BGSLpiMSqoC4UDxp9UsSmeNsI";
 $website = "https://api.telegram.org/bot".$botToken;
 $update = file_get_contents('php://input');
 $update = json_decode($update, TRUE);
